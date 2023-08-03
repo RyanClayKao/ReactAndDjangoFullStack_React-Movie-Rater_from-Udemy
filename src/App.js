f738ed0637aeb59c932a8397be5d54f01437c814
RyanClayKao <f256987584@gmail.com>
2023-08-03 17:37:35 +0800

    使用js的fetch，呼叫Django專案的API，取得movie列表資料(Django專案需要先加好CORS的domain白名單，否則會fetch失敗)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [movies, setMovies] = useState(['Movie 1', 'Movie 2'])
+  const [movies, setMovies] = useState([])
+
+  let tokenFromDjango = 'e515015c76aca083e644f86cb6a80a4fb687b79a';
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/movies/',{
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token ' + tokenFromDjango
+      }
+    }).then(res => res.json())
+      .then(data => {
+        setMovies(data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, [])
 
   return (
     <div className="App">
@@ -12,7 +30,7 @@ function App() {
       <div className="layout">
         <div>
           { movies.map(movie => {
-            return <h2>{movie}</h2>
+            return <h2 key={movie.id}>{movie.title}</h2>
           })}
         </div>
         <div>Movie details</div>
