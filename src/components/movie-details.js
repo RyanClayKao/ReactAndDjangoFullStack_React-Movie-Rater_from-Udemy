6b8721e14572cf93d27ef2c1cc6a98fcc47a5066
RyanClayKao <f256987584@gmail.com>
2023-08-05 19:35:18 +0800

    串接rate_movie API，能夠透過點擊畫面上的星星icon，完成電影給星評價功能

--- src/components/movie-details.js
+++ src/components/movie-details.js
@@ -1,24 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 // import { faStar } from '@fortawesome/free-regular-svg-icons';
 
 function MovieDetails(props) {
+    const movie = props.movie;
+
+    const [highlight, setHighlight] = useState(-1);
+
+    const highlightRate = highIndex => event => {
+        setHighlight(highIndex);
+    }
+
+    const tokenFromDjango = '1408223a9e6c16ca818746457a6905a20532e0c6';
+    const rateClicked = rateNumber => event => {
+        fetch(`http://127.0.0.1:8000/api/movies/${movie.id}/rate_movie/`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Token ' + tokenFromDjango
+            },
+            body: JSON.stringify({'stars': rateNumber})
+        }).then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
     return (
-        <div>
-            {props.movie ? (
+        <React.Fragment>
+            {movie ? (
                 <div>
-                    <h1>{props.movie && props.movie.title}</h1>
-                    <p>{props.movie && props.movie.description}</p>
-                    <FontAwesomeIcon icon={faStar} className={props.movie.avg_rating > 0 ? 'orange': ''}/>
-                    <FontAwesomeIcon icon={faStar} className={props.movie.avg_rating > 1 ? 'orange': ''}/>
-                    <FontAwesomeIcon icon={faStar} className={props.movie.avg_rating > 2 ? 'orange': ''}/>
-                    <FontAwesomeIcon icon={faStar} className={props.movie.avg_rating > 3 ? 'orange': ''}/>
-                    <FontAwesomeIcon icon={faStar} className={props.movie.avg_rating > 4 ? 'orange': ''}/>
-                    ({props.movie.count_of_ratings})
+                    <h1>{movie && movie.title}</h1>
+                    <p>{movie && movie.description}</p>
+                    <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 0 ? 'orange' : ''} />
+                    <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 1 ? 'orange' : ''} />
+                    <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 2 ? 'orange' : ''} />
+                    <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 3 ? 'orange' : ''} />
+                    <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 4 ? 'orange' : ''} />
+                    ({movie.count_of_ratings})
+
+                    <div className="rate-container">
+                        <h2>Rate it</h2>
+                        {[...Array(5)].map((element, index) => {
+                            return (
+                                <FontAwesomeIcon icon={faStar} className={highlight > index - 1 ? 'purple' : ''}
+                                    key={index}
+                                    onMouseEnter={highlightRate(index)}
+                                    onMouseLeave={highlightRate(-1)}
+                                    onClick={rateClicked(index)}
+                                />
+                            )
+                        })}
+                    </div>
                 </div>
             ) : null}
-        </div>
+        </React.Fragment>
     )
 }
 
