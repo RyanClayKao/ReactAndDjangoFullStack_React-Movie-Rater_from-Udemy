50365d5bf8aaa6999a29b5478492de62724c473c
RyanClayKao <f256987584@gmail.com>
2023-08-05 15:37:04 +0800

    設定star icon的顏色樣式及條件套用方式

--- src/components/movie-details.js
+++ src/components/movie-details.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faStar } from '@fortawesome/free-solid-svg-icons';
-import { faStar } from '@fortawesome/free-regular-svg-icons';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+// import { faStar } from '@fortawesome/free-regular-svg-icons';
 
 function MovieDetails(props) {
     return (
@@ -10,7 +10,12 @@ function MovieDetails(props) {
                 <div>
                     <h1>{props.movie && props.movie.title}</h1>
                     <p>{props.movie && props.movie.description}</p>
-                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} className={props.movie.avg_rating > 0 ? 'orange': ''}/>
+                    <FontAwesomeIcon icon={faStar} className={props.movie.avg_rating > 1 ? 'orange': ''}/>
+                    <FontAwesomeIcon icon={faStar} className={props.movie.avg_rating > 2 ? 'orange': ''}/>
+                    <FontAwesomeIcon icon={faStar} className={props.movie.avg_rating > 3 ? 'orange': ''}/>
+                    <FontAwesomeIcon icon={faStar} className={props.movie.avg_rating > 4 ? 'orange': ''}/>
+                    ({props.movie.count_of_ratings})
                 </div>
             ) : null}
         </div>
